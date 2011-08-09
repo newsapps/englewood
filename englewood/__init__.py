6e46b5b04c53675f9b4db25f0c181d1b69e4b8e2
Christopher Groskopf <staringmonkey@gmail.com>
2011-08-09 11:07:59 -0500

    Fix import bug.

--- englewood/__init__.py
+++ englewood/__init__.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python
 
 from dotdensity import DotDensityPlotter
-from masking import mask
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme(fname):
 
 setup(
     name='englewood',
-    version='0.0.7',
+    version='0.0.8',
     description='Tools for deriving different types of maps from shapefiles.',
     long_description=readme('README.rst'),
     author='Christopher Groskopf',
